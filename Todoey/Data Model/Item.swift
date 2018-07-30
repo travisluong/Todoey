e2b008008ec4a90aeb6c7b71ddf2d548a7022224
Travis Luong <travis@zapiary.com>
2018-07-30 15:23:34 -0700

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Travis Luong on 7/30/18.
+//  Copyright © 2018 travisluong. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+}
